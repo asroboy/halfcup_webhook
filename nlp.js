f5165572768dc95260d03429e3e4b8dcb40a01eb
Asrofi Ridho <Asrofi Ridho>
2017-10-06 19:44:15 +0700

    add wit.ai integration

--- nlp.js
+++ nlp.js
@@ -56,11 +56,8 @@ function handleMessage(event, message) {
         getToken(message.nlp.entities['intent'][0].value, event.recipient.id, event.sender.id, false)
 
     } else if (datetime && datetime.confidence > 0.8) {
-        if(grain == 'day'){
-            var msg = 'Today is ' + message.nlp.entities['intent'][0].value;
-            getToken(msg, event.recipient.id, event.sender.id, false)
-        }
-
+        var msg = 'Today is ' + message.nlp.entities['intent'][0].value;
+        getToken(msg, event.recipient.id, event.sender.id, false)
     } else {
         //getDefaultAnswer://halfcup.com/social_rebates_system/wapi/read?token=1234567890&api_name=DEFAULT_ANSWER&page_id=228431964255924
         detDefaultAnswer(event.recipient.id, event.sender.id);
