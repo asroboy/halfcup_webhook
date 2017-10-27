c43349067964e9a5ece75b571dbcbdbdbccc51dc
Asrofi Ridho <Asrofi Ridho>
2017-10-27 17:16:05 +0700

    fix default answer

--- index.js
+++ index.js
@@ -1039,7 +1039,7 @@ function sendEmailForLead(message, page_id) {
 
             var url = 'http://halfcup.com/social_rebates_system/api/sendEmail?' +
                 'sender=noreply@halfcup.com' +
-                '&receiver=asrofiridho@gmail.com' +
+                '&receiver=brotherho@halfcup.com' +
                 '&subject=NEW LEAD RECIEVED : ' + JSON.parse(body).name +
                 '&body=' + message;
             console.log('url', url);
