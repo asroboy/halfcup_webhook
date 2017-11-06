7c0ad80fef75a4e1780af36367d90b5a95bb1537
Asrofi Ridho <Asrofi Ridho>
2017-11-06 09:24:02 +0700

    aggeration object

--- new_nlp.js
+++ new_nlp.js
@@ -271,10 +271,10 @@ function getIndexAggregate(size, pageId, key, aggreationData, recipient, token) 
                 var message = aggreationData[obj.data.mIndex];
 
                 var js_ = JSON.stringify(message);
-                var myEscapedJSONString = js_.escapeSpecialChars();
-                myEscapedJSONString = myEscapedJSONString.replace(/\\\\n/g, "\\n");
-                console.log("TEXT ==> " + myEscapedJSONString);
-                sendMessage(recipient, myEscapedJSONString, token);
+                // var myEscapedJSONString = js_.escapeSpecialChars();
+                // myEscapedJSONString = myEscapedJSONString.replace(/\\\\n/g, "\\n");
+                console.log("TEXT ==> " + js_);
+                sendMessage(recipient, js_, token);
 
                 // res.send(JSON.stringify(obj))
             }
