09d3ff221ad5ebe0b66cdd4092facfe4bd22b430
Asrofi Ridho <Asrofi Ridho>
2018-01-20 09:48:58 +0700

    bug fix

--- index.js
+++ index.js
@@ -396,7 +396,7 @@ app.post('/webhook', function (req, res) {
                             console.log("===== event.postback.payload ========");
                             if (event.postback.payload.indexOf('|param|') > -1) {
                                 var ref = event.postback.payload;
-                                if (ref.indexOf("halfcup.com"){
+                                if (ref.indexOf("halfcup.com") > -1){
                                     ref = ref.replace("http://halfcup.com/social_rebates_system/app/msgredir?", "");
                                 }
                                 ref = ref.split("ref=")[1];
