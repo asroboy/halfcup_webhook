209da0c3a7e2d972e6af6b40d71ac05fb4a75400
Asrofi Ridho <Asrofi Ridho>
2018-03-24 12:10:56 +0700

    true money

--- true_money/true_money.js
+++ true_money/true_money.js
@@ -31,9 +31,61 @@ function getGreetings(keyword){
             } else {
                 var obj = JSON.parse(body);
                 console.log(obj);
+                getToken(obj.data[0]);
             }
         }
     );
 }
 
 
+
+function getToken(message) {
+    var url = 'http://halfcup.com/social_rebates_system/api/getPageMessengerToken?messenger_id=' + sender + '&messenger_uid=' + recipient;
+    console.log('url', url);
+    request({
+            url: url,
+            method: 'GET'
+        }, function (error, response, body) {
+            if (error) {
+                console.log('Error sending message: ', error);
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error);
+            } else {
+                var obj = JSON.parse(body);
+                var code = obj.code;
+
+                if (code == 1) {
+                    var token = obj.messenger_data.pageAccessToken;
+                    var recipient = event.sender.id;
+                    sendMessage(recipient, message, token)
+
+                }
+                if (code == 0) {
+                    console.log('NLP : Can\'t send message, TOKEN NOT FOUND, Get page access token from facebook developer page and register to http://halfcup.com/social_rebates_system');
+
+                }
+            }
+        }
+    );
+}
+
+// generic function sending messages
+function sendMessage(recipientId, message, token) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        } else {
+            // console.log('============ ' + response + ' =========== ');
+        }
+    });
+};
