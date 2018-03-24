2d147eeabdc37eae1655187e9e0d7a0e46c0b92e
Asrofi Ridho <Asrofi Ridho>
2018-03-24 12:52:09 +0700

    true money

--- true_money/true_money.js
+++ true_money/true_money.js
@@ -33,18 +33,15 @@ function getGreetings(event, keyword) {
                 console.log(JSON.stringify(obj.data[0]));
                 var recipient = event.sender.id;
                 var sender = event.recipient.id;
-                var message = JSON.stringify(obj.data[0].message);
-                if (message.indexOf("{{first_name}}")) {
-                    message.replace("{{first_name}}", event.recipient.name.first_name);
-                }
-                getToken(JSON.parse(message), sender, recipient);
+                var messages = JSON.stringify(obj.data);
+                getToken(messages, sender, recipient);
             }
         }
     );
 }
 
 
-function getToken(message, sender, recipient) {
+function getToken(messages, sender, recipient) {
     var url = 'http://halfcup.com/social_rebates_system/api/getPageMessengerToken?messenger_id=' + sender + '&messenger_uid=' + recipient;
     console.log('url', url);
     request({
@@ -61,7 +58,13 @@ function getToken(message, sender, recipient) {
 
                 if (code == 1) {
                     var token = obj.messenger_data.pageAccessToken;
-                    sendMessage(recipient, message, token)
+                    var message = messages[0].message
+                    if (message.indexOf("{{first_name}}")) {
+                        getUserInfo(recipient, token, message)
+                    } else {
+                        sendMessage(recipient, message, token)
+                    }
+
 
                 }
                 if (code == 0) {
@@ -94,3 +97,24 @@ function sendMessage(recipientId, message, token) {
         }
     });
 };
+
+
+function getUserInfo(recipient, token, message) {
+    var url = 'https://graph.facebook.com/v2.6/' + recipient + '?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=' + token;
+    console.log('url', url);
+    request({
+        url: url,
+        method: 'GET'
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        } else {
+            var profile_user = JSON.parse(body);
+            message = message.replace("{{first_name}}", profile_user.first_name);
+            sendMessage(recipient, message, token);
+
+        }
+    });
+}
