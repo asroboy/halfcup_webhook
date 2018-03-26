dc78a384bd36a13a6af2df3cddb334906485a0a0
Asrofi Ridho <Asrofi Ridho>
2018-03-27 05:46:36 +0700

    true money lang

--- true_money/true_money.js
+++ true_money/true_money.js
@@ -240,7 +240,7 @@ function getLanguage(recipient_id, lang, event, keyword) {
 
 function savePreviousActions(recipient_id, type, keyword) {
     if(keyword.indexOf('prev_action=') > -1){
-        keyword = keyword.split('prev_action=')[0]
+        keyword = keyword.split('||prev_action=')[0]
     }
     var url = urlApiSaveOrUpdate + "?api_name=prev_actions&key=" + keyword + "&recipient_id=" + recipient_id + '&type=' + type;
     console.log(url)
