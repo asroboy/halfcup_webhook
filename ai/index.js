4914b069119c5a2575ae6cad178b6f5efb77c290
Asrofi Ridho <Asrofi Ridho>
2018-07-13 07:47:18 +0700

    add phone query

--- ai/index.js
+++ ai/index.js
@@ -1532,7 +1532,7 @@ function get_tracking_id(aggregation, messenger_id, page_id, textOrPhone, isPhon
             console.log('==> GET TRACKING ID RESULT :', JSON.stringify(obj));
             if (obj.data !== null) {
                 if (isPhone) {
-                    new_nlp.phoneAction(textOrPhone, messenger_id, page_id, res);
+                    new_nlp.phoneAction(textOrPhone, page_id, messenger_id, res);
                     register_tracking_phone(obj.data.tracking_id, textOrPhone);
                 } else {
                     register_tracking(obj.data.tracking_id, textOrPhone);
