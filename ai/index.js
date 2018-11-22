6897af4b97027eb4c18ffb3069d5163fb1a70fc8
Asrofi Ridho <Asrofi Ridho>
2018-11-22 17:22:40 +0700

    update send whatsapp api

--- ai/index.js
+++ ai/index.js
@@ -941,7 +941,7 @@ function getPageAccessTokenForLead(sender, message, leadgenId, formId, emailMess
                 if (code == 1) {
                     var token = obj.messenger_data.pageAccessToken;
                     var agentEmail = obj.email;
-                    var agenMobile = obj.mobile;
+                    var agenMobile = "+65" + obj.mobile;
                     var recipientId = obj.messenger_data.adminMessengerId;
                     // var longLiveToken = "EAABqJD84pmIBAP4xtPj3NTLfCzWp17iZByoFndpbnEq79ZAOGs7XdF5YMO5i1GgQ3zHex200f2uvLHWqzFxRk0RrC1jV7RZBZAqtU2mLluefhmexnX7SSnTP63Hy2x3AAvv5FgkU48FE95fpj7c8ZBREHJIVBYg4ZD";
                     var longLiveToken = "EAABqJD84pmIBAP6U37LseOrNLP6Xt13zCRR8dUCcNS4T1tKFQd8JZAyGQJOPq4mOfHazyppWRGYQaO2aaT1vQA4HNSEu10D6CgH220ND9ecweec3WOMGsvbIMv1gzJI5NrYXRKf5Nqmc8o9cfJdG9eBeU1UZBuOK2iSZCBlogZDZD";
